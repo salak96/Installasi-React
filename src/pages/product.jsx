19198d9d71f6c7599a4d91709de502320162b4f6
lambang <sasangkakurniawan.com>
2023-05-30 18:58:13 +0700

    Arraymaplist

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,21 +1,45 @@
 import CardProduct from '../components/Fragments/CardProduct';
+const products = [
+    {
+        id: 1,
+        title: 'Sepatu Baru',
+        description: 'Sepatu baru dengan kualitas terbaik',
+        price: 'Rp.4.500.000',
+        image: '/images/sepatu.jpg',
+    },
+    {
+        id: 2,
+        title: 'Sepatu Baru 2',
+        description: 'Sepatu baru dengan kualitas terbaik',
+        price: 'Rp550.000',
+        image: '/images/sepatu.jpg',
+    },
+    {
+        id: 3,
+        title: 'Sepatu Baru 3',
+        description: 'Sepatu baru dengan kualitas terbaik',
+        price: 'Rp 1.500.000',
+        image: '/images/sepatu.jpg',
+    },
+    {
+        id: 4,
+        title: 'Sepatu Baru 4',
+        description: 'Sepatu baru dengan kualitas terbaik',
+        price: 'Rp 1.500.000',
+        image: '/images/sepatu.jpg',
+    },
 
+]
 const product = () => {
     return (
         <div className='flex justify-center py-5'>
-            <CardProduct>
-                <CardProduct.Header image={'/images/sepatu.jpg'} />
-                <CardProduct.Body title='Sepatu Baru'>Sepatu baru dengan kualitas terbaik</CardProduct.Body>
-
-                <CardProduct.Footer price='Rp.4.500.000' />
-            </CardProduct>
-
-            <CardProduct>
-                <CardProduct.Header image={'/images/sepatu2.jpeg'} />
-                <CardProduct.Body title='Sepatu Baru'>Sepatu nike </CardProduct.Body>
-
-                <CardProduct.Footer price='Rp.4.500.000' />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body title={product.title}>{product.description}</CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     );
 };
